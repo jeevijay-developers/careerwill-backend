3d89f0ba1f8e2296bcd5c1ed543139a95468429f
VishalBala@Office-Jeevijay <vishal.bala.100@gmail.com>
2025-07-14 16:33:14 +0530

    feat: configure CORS to allow specific origins for API requests

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ const connectDB = require("./config/mongoDB");
 const dotenv = require("dotenv").config();
 const authRoute = require("./router/AuthRouter");
 const kitRoute = require("./router/kitRoutes");
+const cors = require("cors");
 
+const ORIGINS = [
+  "http://localhost:3000",
+  "https://careerwill-frontend.vercel.app",
+  "https://careerwill-frontend-git-main-cw-ashishverma.vercel.app",
+];
 const app = express();
 const PORT = process.env.PORT || 5000;
+const CORS = {
+  origin: (origin, callback) => {
+    if (ORIGINS.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(CORS));
 connectDB();
 app.use(express.json());
 app.use("/api/kit", kitRoute);

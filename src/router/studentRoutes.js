c81bfd1fcc7fb7f4da5d505b03f64751e350e5a7
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-07-19 10:48:38 +0530

    spell correction

--- src/router/studentRoutes.js
+++ src/router/studentRoutes.js
@@ -12,8 +12,8 @@ router.post("/create-student", createStudent);
 
 router.post("/upload-image", upload, uploadImage);
 
-router.get("/get-all-sutdents", getAllStudents);
-
+router.get("/get-all-students", getAllStudents);
+router.get("/get-student-by-batch/:batchId", getStudentById);
 router.get("/get-student-by-id/:id", getStudentById);
 
 router.get("/find-parent-by-email/:email", findParentByEmail);
